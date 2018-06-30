ed6e3a7d6cf1cdce22f6e680945caf89084a54bf
mingyan.yu <mingyan.yu@wormpex.com>
2018-06-30 17:56:51 +0800

    feat:init

---
+++ index.js
@@ -0,0 +1,13 @@
+import route from './src/route';
+
+/**
+ * 
+ * @param {traverse diractory} directory
+ * @param {whether subdirectories should be searched } useSubdirectories
+ * @param {match files} targetRegExp
+ * @param {excluded files} excludeRegExp
+ * @param {whether} isForceLowercase
+ */
+export default function (directory, useSubdirectories = true, targetRegExp, excludeRegExp, isForceLowercase = true) {
+
+}
